27bf1f59d7ebf0f7e3f4d295862c26b676cee599
Lee Martin <lee.martin@pliminus.co.uk>
2021-07-17 20:36:44 +0100

    Added: Basic nav menu sidebar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,46 +1,114 @@
-import { CssBaseline } from "@material-ui/core";
+import { AppBar, CssBaseline, Divider, Drawer, IconButton, List, ListItem, Toolbar, Typography } from "@material-ui/core";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from 'next/router'
 import React from "react";
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 interface NavigationPage {
   path: string,
   text: string,
 }
 const NavigationPages: NavigationPage[] = [
-  {path:"/about", text: "About"},
-  {path:"/bespoke-arrangements", text: "Bespoke Arrangements"},
-  {path:"/contact", text: "Contact"},
-  {path:"/cut-flowers", text: "Cut Flowers"},
-  {path:"/events", text: "Events"},
-  {path:"/", text: "Home"},
-  {path:"/petal-confetti", text: "Petal Confetti"},
-  {path:"/workshops", text: "Workshops"}
+  { path: "/about", text: "About" },
+  { path: "/bespoke-arrangements", text: "Bespoke Arrangements" },
+  { path: "/contact", text: "Contact" },
+  { path: "/cut-flowers", text: "Cut Flowers" },
+  { path: "/events", text: "Events" },
+  { path: "/", text: "Home" },
+  { path: "/petal-confetti", text: "Petal Confetti" },
+  { path: "/workshops", text: "Workshops" }
 ]
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  menuButton: {
+    marginLeft: theme.spacing(1),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+}));
+
 export default function Header() {
-      const router = useRouter();
-      const currentPage = router.pathname;
+  const router = useRouter();
+  const currentPage = router.pathname;
+  const classes = useStyles();
+  const theme = useTheme();
 
-      return (   
-        <div>
-          <div>
-            <ul>       
-              {NavigationPages.map((navlink, i) => navigationLink(currentPage, navlink, i))}
-            </ul>
-          </div>
-        </div>   
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-      )
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+        className={classes.menuButton}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+
+        <List>
+          {NavigationPages.map((navlink, i) => navigationLink(currentPage, navlink, i))}
+        </List>
+      </Drawer>
+    </>
+  )
 }
 
 function navigationLink(currentPage: string, navlink: NavigationPage, key: number) {
-    return (
-      <li key={key}>
-        <Link href={navlink.path}>
-          <a style={(currentPage == navlink.path)? {backgroundColor: "red"}: undefined}>{navlink.text}</a>
-        </Link>
-      </li>
-      ) 
+  return (
+    <Link href={navlink.path}>
+      <ListItem button key={key} style={(currentPage == navlink.path) ? { backgroundColor: "rgba(0,0,0,0.1)" } : undefined}>
+        <a>{navlink.text}</a>
+      </ListItem>
+    </Link>
+  )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../components/layout/layout'
+import { ThemeProvider } from '@material-ui/core'
+import { createTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: blue,
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
     )
 }
 export default MyApp
