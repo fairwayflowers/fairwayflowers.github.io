2c05720ed2bf0cf89d2bc645c6f8c2acb63c7d7a
Lee Martin <lee.martin@pliminus.co.uk>
2021-07-17 17:37:44 +0100

    Fix: Error from CssBaseline position

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Head from 'next/head'
-import { CssBaseline } from '@material-ui/core'
+import { Container, CssBaseline } from '@material-ui/core'
 import Header from '../header'
 
 export const Layout: React.FunctionComponent = props =>
@@ -10,10 +10,12 @@ export const Layout: React.FunctionComponent = props =>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
-      <CssBaseline />
-    </Head>
+    </Head>      
+    <CssBaseline />
     <Header/>
     <main>
-      {props.children}
+      <Container>
+        {props.children ?? <></>}
+      </Container>
     </main>
   </div>
