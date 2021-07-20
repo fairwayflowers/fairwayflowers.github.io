422c8a07b4d0b92e87c622ae9e7dd28646898ad6
Lee Martin <lee.martin@pliminus.co.uk>
2021-07-20 22:21:40 +0100

    Added: Feature image component and home page basic layout

--- src/components/feature-image.tsx
+++ src/components/feature-image.tsx
@@ -1,4 +1,5 @@
 import styles from './FeatureImage.module.css'
+import { Image } from 'react-bootstrap'
 
 interface FeatureImageProps {
     image: string,
@@ -8,7 +9,7 @@ interface FeatureImageProps {
 export default function FeatureImage(props: FeatureImageProps) {
     return (
     <div className={styles.headerImageContainer}>
-        <img src={props.image} className={styles.headerImage} />
+        <Image src={props.image} fluid />
         {props.children != undefined &&
             <div className={styles.headerImageBanner}>
                 {props.children ?? <></>}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import React from 'react'
+import { Col, Container, Row, Image } from 'react-bootstrap'
 import FeatureImage from '../components/feature-image'
 
 export default function Home() {
@@ -14,27 +15,42 @@ export default function Home() {
           <h2>Cut Flowers, Petal Confetti and Arrangements</h2>
         </>
       </FeatureImage>
-      <h3>Fairway Flowers gives individual care and attention to all your floral visions.</h3>
-      <div>
-        <h4>Cut Flowers</h4>
-        <p>Hand selected from our private cutting garden, grown in the heart of the Cotswolds.</p>
-      </div>
-      <div>
-        <h4>Petal Confetti</h4>
-        <p>Biodegradable and environmentally friendly confetti to enhance your special day.</p>
-      </div>
-      <div>
-        <h4>Bespoke Arrangements</h4>
-        <p>Your unique floral vision created in stunning seasonal flowers.</p>
-      </div>
-      <div>
-        <h4>Workshops</h4>
-        <p>One-to-one or Group Workshops</p>
-      </div>
-      <div>
-        <h4>Events</h4>
-        <p>We can attend your events year round.</p>
-      </div>
+      <Container>
+        <Row className="justify-content-sm-center">
+          <Col sm={8} className="text-center">
+            <h3>Fairway Flowers gives individual care and attention to all your floral visions.</h3>
+          </Col>
+        </Row>
+        <Row className="justify-content-sm-center text-center">
+          <Col sm={4}>
+            <Image src="/images/cut_flowers.JPG" rounded fluid/>
+            <h4>Cut Flowers</h4>
+            <p>Hand selected from our private cutting garden, grown in the heart of the Cotswolds.</p>
+          </Col>
+          <Col sm={4}>
+            <Image src="/images/petal_confetti.jpg" rounded fluid/>
+            <h4>Petal Confetti</h4>
+            <p>Biodegradable and environmentally friendly confetti to enhance your special day.</p>
+          </Col>
+          <Col sm={4}>
+            <Image src="/images/bespoke_arrangements.jpg" rounded fluid/>
+            <h4>Bespoke Arrangements</h4>
+            <p>Your unique floral vision created in stunning seasonal flowers.</p>
+          </Col>
+        </Row>
+        <Row className="justify-content-sm-center text-center">
+          <Col sm={4}>
+            <Image src="/images/workshops.jpg" rounded fluid/>
+            <h4>Workshops</h4>
+            <p>One-to-one or Group Workshops</p>
+          </Col>
+          <Col sm={4}>
+            <Image src="/images/events.jpg" rounded fluid/>
+            <h4>Events</h4>
+            <p>We can attend your events year round.</p>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
