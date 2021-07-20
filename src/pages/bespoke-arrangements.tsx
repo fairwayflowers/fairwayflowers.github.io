32f6a661e5cf3b07dd5d8043c47c952d382105f1
Lee Martin <lee.martin@pliminus.co.uk>
2021-07-20 23:05:17 +0100

    Update: Quick starter for 10 of a gallery

--- src/pages/bespoke-arrangements.tsx
+++ src/pages/bespoke-arrangements.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import React from 'react'
+import { Col, Container, Row, Image } from 'react-bootstrap'
+
+
 
 export default function BespokeArrangements() {
   return (
@@ -7,10 +10,29 @@ export default function BespokeArrangements() {
       <Head>
         <title>Fairway Flowers - Bespoke Arrangements</title>
       </Head>
-      <h1>Bespoke Arrangements</h1>
-      <p>Your unique floral vision created in stunning seasonal flowers - birthdays, weddings, sympathy or just because.</p>
-      <p>We can provide a unique service for sympathy flowers where we can pick and arrange from your own garden.</p>
-      <p>Get in touch to order your arrangements today.</p>
+      <Container>
+        <Row className="justify-content-sm-center">
+          <Col sm={8}>
+            <h1>Bespoke Arrangements</h1>
+            <p>Your unique floral vision created in stunning seasonal flowers - birthdays, weddings, sympathy or just because.</p>
+            <p>We can provide a unique service for sympathy flowers where we can pick and arrange from your own garden.</p>
+            <p>Get in touch to order your arrangements today.</p>
+          </Col>
+        </Row>
+        <Row>
+          {[...Array(36).keys()].map((i: number) => galleryImage(i))}
+        </Row>
+      </Container>
     </>
+  )
+}
+
+function galleryImage(key: number) {
+  let rawKey = key.toString();
+  let imagePath = "/images/bespoke-arrangements/gallery/" + rawKey.padStart(4 ,"0") + ".jpg";
+  return (
+    <Col sm={3}>
+      <Image src={imagePath} thumbnail/>
+    </Col>
   )
 }
