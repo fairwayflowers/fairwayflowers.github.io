9539d477fc4eec06748598af0fa28a6d261c06e8
Lee Martin <lee.martin@pliminus.co.uk>
2021-07-27 21:04:57 +0100

    Added: Index page links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { Col, Container, Row } from 'react-bootstrap'
 import Image from 'next/image'
 import FeatureImage from '../components/feature-image'
 import styles from './index.module.css'
+import helperStyles from '../components/helper.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -25,24 +27,40 @@ export default function Home() {
         </Row>
         <Row className="my-4 justify-content-sm-center text-center">
           <Col sm={4}>
-            {IndexImage("/images/cut_flowers.JPG", "Cut Flowers")}
-            <h4>Cut Flowers</h4>
-            <p>Hand selected from our private cutting garden, grown in the heart of the Cotswolds.</p>
+            <Link href="/cut-flowers">
+              <a className={helperStyles.linkUnstyled}>
+                {IndexImage("/images/cut_flowers.JPG", "Cut Flowers")}
+                <h4>Cut Flowers</h4>
+                <p>Hand selected from our private cutting garden, grown in the heart of the Cotswolds.</p>
+              </a>
+            </Link>
           </Col>
           <Col sm={4}>
-            {IndexImage("/images/petal_confetti.jpg", "Petal Confetti")}
-            <h4>Petal Confetti</h4>
-            <p>Biodegradable and environmentally friendly confetti to enhance your special day.</p>
+            <Link href="/petal-confetti">
+              <a className={helperStyles.linkUnstyled}>
+                {IndexImage("/images/petal_confetti.jpg", "Petal Confetti")}
+                <h4>Petal Confetti</h4>
+                <p>Biodegradable and environmentally friendly confetti to enhance your special day.</p>
+              </a>
+            </Link>
           </Col>
           <Col sm={4}>
-            {IndexImage("/images/bespoke_arrangements.jpg", "Bespoke Arrangements")}
-            <h4>Bespoke Arrangements</h4>
-            <p>Your unique floral vision created in stunning seasonal flowers.</p>
+            <Link href="/bespoke-arrangements">
+              <a className={helperStyles.linkUnstyled}>
+                {IndexImage("/images/bespoke_arrangements.jpg", "Bespoke Arrangements")}
+                <h4>Bespoke Arrangements</h4>
+                <p>Your unique floral vision created in stunning seasonal flowers.</p>
+              </a>
+            </Link>
           </Col>
           <Col sm={4}>
-            {IndexImage("/images/workshops.jpg", "Workshop")}
-            <h4>Workshops</h4>
-            <p>One-to-one or Group Workshops</p>
+            <Link href="/workshops">
+              <a className={helperStyles.linkUnstyled}>
+                {IndexImage("/images/workshops.jpg", "Workshop")}
+                <h4>Workshops</h4>
+                <p>One-to-one or Group Workshops</p>
+              </a>
+            </Link>
           </Col>
         </Row>
       </Container>
